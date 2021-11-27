86bcfc10fcad63142e25a5abf8d5a2c31a11568d
Roland Treiber <rolandtreiber@gmail.com>
2021-11-27 07:19:24 +0000

    Attributes list

---
+++ src/components/product/product-attributes-table.js
@@ -0,0 +1,207 @@
+import React, {useContext} from 'react'
+import {SettingsContext} from "../../contexts/settings-context";
+import {
+    Box,
+    Checkbox,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    TableSortLabel,
+    Typography,
+    Link, Skeleton, Divider
+} from "@material-ui/core";
+import {Scrollbar} from "../scrollbar";
+import { Link as RouterLink } from 'react-router-dom';
+import {ProductMenu} from "./product-menu";
+import {ResourceError} from "../resource-error";
+import {ResourceUnavailable} from "../resource-unavailable";
+import {Pagination} from "../pagination";
+import {Status} from "../status";
+import {format} from "date-fns";
+
+const columns = [
+    {
+        id: 'name',
+        label: 'Name',
+        translatable: true
+    },
+    {
+        id: 'updated_at',
+        label: 'Updated'
+    },
+    {
+        id: 'enabled',
+        label: 'enabled'
+    }
+];
+
+const types = ['text', 'image', 'color']
+
+const statusVariants = [
+    {
+        color: 'success.main',
+        label: 'Enabled',
+        value: true
+    },
+    {
+        color: 'error.main',
+        label: 'Disabled',
+        value: false
+    }
+];
+
+const ProductAttributesTable = (props) => {
+    const {
+        error,
+        isLoading,
+        onPageChange,
+        onSelect,
+        onSelectAll,
+        onSortChange,
+        page,
+        pagesCount,
+        data,
+        selectedElements,
+        sort,
+        sortBy
+    } = props;
+
+    const displayLoading = isLoading;
+    const displayError = Boolean(!isLoading && error);
+    const displayUnavailable = Boolean(!isLoading && !error && !data.length);
+    const {language} = useContext(SettingsContext)
+
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flex: 1,
+                flexDirection: 'column'
+            }}
+        >
+            <Scrollbar>
+                <Table sx={{minWidth: 800}}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell padding="checkbox">
+                                <Checkbox
+                                    checked={data.length > 0
+                                    && selectedElements.length === data.length}
+                                    disabled={isLoading}
+                                    indeterminate={selectedElements.length > 0
+                                    && selectedElements.length < data.length}
+                                    onChange={onSelectAll}
+                                />
+                            </TableCell>
+                            {columns.map((column) => (
+                                <TableCell key={column.id}>
+                                    <TableSortLabel
+                                        active={sortBy === column.id}
+                                        direction={sortBy === column.id ? sort : 'asc'}
+                                        disabled={isLoading}
+                                        onClick={(event) => onSortChange(event, column.id, column.translatable)}
+                                    >
+                                        {column.label}
+                                    </TableSortLabel>
+                                </TableCell>
+                            ))}
+                            <TableCell/>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data.map(attribute => {
+                            const statusVariant = statusVariants.find((variant) => variant.value
+                                === attribute.enabled);
+
+                            return (<TableRow
+                                    hover
+                                    key={attribute.id}
+                                    selected={!!selectedElements.find((selectedCustomer) => selectedCustomer
+                                        === attribute.id)}
+                                >
+                                    <TableCell padding="checkbox">
+                                        <Checkbox
+                                            checked={!!selectedElements.find((selectedCustomer) => selectedCustomer
+                                                === attribute.id)}
+                                            onChange={(event) => onSelect(event, attribute.id)}
+                                        />
+                                    </TableCell>
+                                    <TableCell>
+                                        <Box
+                                            sx={{
+                                                alignItems: 'center',
+                                                display: 'flex'
+                                            }}
+                                        >
+                                            <Link
+                                                color="inherit"
+                                                component={RouterLink}
+                                                sx={{ display: 'block' }}
+                                                to={"/dashboard/product-attributes/"+attribute.id}
+                                                underline="none"
+                                                variant="subtitle2"
+                                            >
+                                                {attribute.name[language]}
+                                            </Link>
+                                        </Box>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Typography
+                                            color="inherit"
+                                            variant="body2"
+                                        >
+                                            {format(new Date(attribute.updated_at), 'dd MMM yyyy')}
+                                        </Typography>
+                                    </TableCell>
+                                    <TableCell>
+                                        <Status
+                                            color={statusVariant.color}
+                                            label={statusVariant.label}
+                                        />
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        <ProductMenu />
+                                    </TableCell>
+                                </TableRow>
+
+                            )})}
+                    </TableBody>
+                </Table>
+            </Scrollbar>
+            {displayLoading && (
+                <Box sx={{ p: 2 }}>
+                    <Skeleton height={42} />
+                    <Skeleton height={42} />
+                    <Skeleton height={42} />
+                </Box>
+            )}
+            {displayError && (
+                <ResourceError
+                    error={error}
+                    sx={{
+                        flexGrow: 1,
+                        m: 2
+                    }}
+                />
+            )}
+            {displayUnavailable && (
+                <ResourceUnavailable
+                    sx={{
+                        flexGrow: 1,
+                        m: 2
+                    }}
+                />
+            )}
+            <Divider sx={{ mt: 'auto' }} />
+            <Pagination
+                disabled={isLoading}
+                onPageChange={onPageChange}
+                page={page}
+                pagesCount={pagesCount}
+            />
+        </Box>)
+}
+
+export default ProductAttributesTable
