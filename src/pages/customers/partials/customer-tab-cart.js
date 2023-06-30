ade975dd1243666d4501eec2cb6d120a0d789884
Roland Treiber <info@thecaringdeveloper.com>
2023-06-30 08:33:51 +0100

    Customer tab cart added

--- src/pages/customers/partials/customer-tab-cart.js
+++ src/pages/customers/partials/customer-tab-cart.js
@@ -1,9 +1,125 @@
 import React from "react"
+import {
+  Avatar,
+  Box,
+  Card,
+  Grid, Link,
+  List,
+  ListItemText,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow, Typography
+} from "@material-ui/core";
+import {Scrollbar} from "../../../components/common/scrollbar";
+import {useLanguage} from "../../../hooks/use-language";
+import Price from "../../../components/common/price";
+import ListItemGridKeyValue from "../../../components/common/list-item-grid-key-value";
+import {format} from "date-fns";
+import {Link as RouterLink} from "react-router-dom";
+import NoImg from "../../../static/images/no-image.png";
 
 const CustomerTabCart = ({data}) => {
-  return (
-    <h1>Cart</h1>
-  )
+  const {getLang} = useLanguage()
+
+  return (<Grid container spacing={2}>
+      {data.products.length > 0 ? <>
+        <Grid item xs={12} lg={6}>
+          <List>
+            <ListItemGridKeyValue
+              left={<ListItemText>Last Updated</ListItemText>}
+              right={<ListItemText>{format(new Date(data.last_updated), 'dd-MMM-yyyy HH:mm')}</ListItemText>}
+            />
+            <ListItemGridKeyValue
+              left={<ListItemText>Products Total</ListItemText>}
+              right={<ListItemText>{data.products_total}</ListItemText>}
+            />
+          </List>
+          </Grid>
+        <Grid item xs={12} lg={6}>
+          <List>
+            <ListItemGridKeyValue
+              left={<ListItemText>Price Payable</ListItemText>}
+              right={<ListItemText><Price>{data.price}</Price></ListItemText>}
+            />
+            <ListItemGridKeyValue
+              left={<ListItemText>Full Price (without discounts)</ListItemText>}
+              right={<ListItemText><Price>{data.full_price}</Price></ListItemText>}
+            />
+          </List>
+        </Grid>
+        <Grid item xs={12} lg={12}>
+          <Box
+            sx={{
+              display: 'flex', flex: 1, flexDirection: 'column'
+            }}
+          >
+            <Scrollbar>
+              <Table sx={{minWidth: 1000}}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell padding="normal">Name</TableCell>
+                    <TableCell padding="normal">Quantity</TableCell>
+                    <TableCell padding="normal">Unit price</TableCell>
+                    <TableCell padding="normal">Price</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.products.map(product => (<TableRow key={product.id}>
+                      <TableCell
+                        padding="normal">
+                        <Box
+                          sx={{
+                            alignItems: 'center',
+                            display: 'flex'
+                          }}
+                        >
+                          <Avatar
+                            alt={getLang(product.name)}
+                            src={product.cover_photo_url ? product.cover_photo_url : NoImg}
+                            sx={{
+                              width: 64,
+                              height: 64
+                            }}
+                            variant="rounded"
+                          />
+                          <Box sx={{ ml: 2 }}>
+                            <Link
+                              color="inherit"
+                              component={RouterLink}
+                              sx={{ display: 'block' }}
+                              to={"/products/"+product.id}
+                              underline="none"
+                              variant="subtitle2"
+                            >
+                              {getLang(product.name)}
+                            </Link>
+                            <Typography
+                              color="textSecondary"
+                              sx={{ mt: 1 }}
+                              variant="body2"
+                            >
+                              {product.variant ? getLang(product.variant.name) : getLang(product.name)}
+                            </Typography>
+                          </Box>
+                        </Box>
+                      </TableCell>
+                      <TableCell padding="normal">{product.quantity}</TableCell>
+                      <TableCell padding="normal"><Price>{product.item_final_price}</Price></TableCell>
+                      <TableCell padding="normal"><Price>{product.price}</Price></TableCell>
+                    </TableRow>))}
+                </TableBody>
+              </Table>
+            </Scrollbar>
+          </Box>
+        </Grid>
+      </> : (<Grid item xs={12}>
+          <Card variant="outlined" style={{padding: 10}}>
+            No products to display
+          </Card>
+        </Grid>)}
+    </Grid>)
 }
 
 export default CustomerTabCart
--- src/pages/customers/partials/customer-tab-orders.js
+++ src/pages/customers/partials/customer-tab-orders.js
@@ -14,102 +14,74 @@ const CustomerTabOrders = ({data}) => {
   const theme = useTheme()
   const {getLang} = useLanguage()
 
-  return (
-    <Grid container spacing={2}>
-      {data.length > 0 ? data.map(order => (
-          <Grid key={order.id} item xs={12} lg={6}>
-            <Card variant="outlined">
-              <Grid container spacing={2}>
-                <Grid item xs={6}>
-                  <FullWidthSquareBox style={{
-                    borderRadius: theme.shape.borderRadius,
-                    backgroundColor: theme.palette.neutral[200],
-                    minWidth: 210
-                  }}>
-                    <Box sx={{position: "absolute", top: 0, left: 10}}>
-                      <p>{statuses[order.status].label}</p>
-                    </Box>
-                    <Grid container sx={{position: "absolute", bottom: 10, left: 10}}>
-                      <Grid item xs={3}>
-                        <IconButton aria-label="download" size={"small"}>
-                          <Download/>
-                        </IconButton>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <IconButton aria-label="email" size={"small"}>
-                          <Email/>
-                        </IconButton>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <IconButton aria-label="print" size={"small"}>
-                          <Print/>
-                        </IconButton>
-                      </Grid>
-                      <Grid item xs={3}>
-                        <IconButton aria-label="locate" size={"small"}>
-                          <NorthEast/>
-                        </IconButton>
-                      </Grid>
-                    </Grid>
-                  </FullWidthSquareBox>
+  return (<Grid container spacing={2}>
+    {data.length > 0 ? data.map(order => (<Grid key={order.id} item xs={12} lg={6}>
+      <Card variant="outlined">
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <FullWidthSquareBox style={{
+              borderRadius: theme.shape.borderRadius, backgroundColor: theme.palette.neutral[200], minWidth: 210
+            }}>
+              <Box sx={{position: "absolute", top: 0, left: 10}}>
+                <p>{statuses[order.status].label}</p>
+              </Box>
+              <Grid container sx={{position: "absolute", bottom: 10, left: 10}}>
+                <Grid item xs={3}>
+                  <IconButton aria-label="download" size={"small"}>
+                    <Download/>
+                  </IconButton>
                 </Grid>
-                <Grid item xs={6}>
-                  <List>
-                    <ListItemGridKeyValue
-                      left={
-                        <ListItemText>Placed</ListItemText>
-                      }
-                      right={
-                        <ListItemText>{format(new Date(order.created_at), 'dd-MMM-yyyy HH:mm')}</ListItemText>
-                      }
-                    />
-                    <ListItemGridKeyValue
-                      left={
-                        <ListItemText>Total</ListItemText>
-                      }
-                      right={
-                        <ListItemText>{order.total_price}</ListItemText>
-                      }
-                    />
-                    <ListItemGridKeyValue
-                      left={
-                        <ListItemText>Discount</ListItemText>
-                      }
-                      right={
-                        <ListItemText>{order.total_discount}</ListItemText>
-                      }
-                    />
-                    <ListItemGridKeyValue
-                      left={
-                        <ListItemText>Delivery</ListItemText>
-                      }
-                      right={
-                        <ListItemText><Price>{order.delivery_cost}</Price> ({getLang(order.delivery_type)}))</ListItemText>
-                      }
-                    />
-                    <ListItemGridKeyValue
-                      left={
-                        <ListItemText>Town</ListItemText>
-                      }
-                      right={
-                        <ListItemText>{order.town}</ListItemText>
-                      }
-                    />
-                  </List>
+                <Grid item xs={3}>
+                  <IconButton aria-label="email" size={"small"}>
+                    <Email/>
+                  </IconButton>
+                </Grid>
+                <Grid item xs={3}>
+                  <IconButton aria-label="print" size={"small"}>
+                    <Print/>
+                  </IconButton>
+                </Grid>
+                <Grid item xs={3}>
+                  <IconButton aria-label="locate" size={"small"}>
+                    <NorthEast/>
+                  </IconButton>
                 </Grid>
               </Grid>
-            </Card>
+            </FullWidthSquareBox>
           </Grid>
-        )
-      ) : (
-        <Grid item xs={12}>
-          <Card variant="outlined" style={{padding: 10}}>
-            No orders to display
-          </Card>
+          <Grid item xs={6}>
+            <List>
+              <ListItemGridKeyValue
+                left={<ListItemText>Placed</ListItemText>}
+                right={<ListItemText>{format(new Date(order.created_at), 'dd-MMM-yyyy HH:mm')}</ListItemText>}
+              />
+              <ListItemGridKeyValue
+                left={<ListItemText>Total</ListItemText>}
+                right={<ListItemText>{order.total_price}</ListItemText>}
+              />
+              <ListItemGridKeyValue
+                left={<ListItemText>Discount</ListItemText>}
+                right={<ListItemText>{order.total_discount}</ListItemText>}
+              />
+              <ListItemGridKeyValue
+                left={<ListItemText>Delivery</ListItemText>}
+                right={
+                  <ListItemText><Price>{order.delivery_cost}</Price> ({getLang(order.delivery_type)}))</ListItemText>}
+              />
+              <ListItemGridKeyValue
+                left={<ListItemText>Town</ListItemText>}
+                right={<ListItemText>{order.town}</ListItemText>}
+              />
+            </List>
+          </Grid>
         </Grid>
-      )}
-    </Grid>
-  )
+      </Card>
+    </Grid>)) : (<Grid item xs={12}>
+      <Card variant="outlined" style={{padding: 10}}>
+        No orders to display
+      </Card>
+    </Grid>)}
+  </Grid>)
 }
 
 export default CustomerTabOrders
