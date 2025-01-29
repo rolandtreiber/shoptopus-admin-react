f686fc7aadaa558fa74dcf577899eb09782a0101
GaborVxxx <gaborvtattoo@gmail.com>
2025-01-29 17:23:17 +0000

    gabor_local_admin_branch

--- src/components/common/category-tree-select.js
+++ src/components/common/category-tree-select.js
@@ -74,7 +74,7 @@ const CategoryTreeSelect = ({categories, selection = [], setSelection}) => {
     <>
       {items.map(c => (
         <TreeItem ContentComponent={TreeItemContent} value={c.id} key={c.id} nodeId={c.id} label={getLang(c.name)}>
-          {c.children.length > 0 && renderTreeItems(c.children)}
+          {c?.children?.length > 0 && renderTreeItems(c.children)}
         </TreeItem>
       ))}
     </>
